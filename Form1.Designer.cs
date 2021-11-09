098b32fa01bdfdea91a5e7d8057c12466c66358c
noura <1701595@eng.asu.edu.eg>
2021-11-10 01:52:55 +0200

    final isa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace WindowsFormsApp1
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -50,10 +51,11 @@ namespace WindowsFormsApp1
             // 
             // richTextBox1
             // 
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 60);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(380, 378);
+            this.richTextBox1.Size = new System.Drawing.Size(380, 584);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -67,19 +69,32 @@ namespace WindowsFormsApp1
             this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.button2.Font = new System.Drawing.Font("Lucida Bright", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Navy;
-            this.button2.Location = new System.Drawing.Point(408, 12);
+            this.button2.Location = new System.Drawing.Point(660, 11);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(380, 42);
+            this.button2.Size = new System.Drawing.Size(394, 42);
             this.button2.TabIndex = 3;
             this.button2.Text = "Show Tokens ";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(660, 60);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(394, 617);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1227, 648);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
@@ -95,6 +110,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,26 +13,56 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        enum states { START, COMMENT, NUM, IDINTIFIER, ERROR,ASSIGN, DONE };
+
+        /********************Globel variable ****************/
+        states state = states.START;
+        int j = 0;
+        string after;
+        /********************Function Definitions****************/
+
+        /* if the input is withen the range of digits (0 to 9) this function returns true,
+         * and returns false if not */
+        bool isDigit(char d) { return (d >= '0' && d <= '9'); }
+
+        /* if the input is withen the range of alphapetic letters (lowercase or uppercase) this function returns true,
+         * and returns false if not */
+        bool isLetter(char l) { return (l >= 'a' && l <= 'z' || l >= 'A' && l <= 'Z'); }
+
+        /* if the input is one of those symbols: '+' , '-' , '*' , '/','*' =, '<' , '(' , ')' ,';' this function returns true,
+         * and returns false if not */
+        bool isSymbol(char c)
+        {
+            return (c == '+' || c == '-' || c == '*' || c == '/' || c == '=' || c == '<' || c == '>' ||
+                c == '(' || c == ')' || c == ';');
+        }
+        /* if the input is either space, tab, or new line, it is considered white space and this function returns true,
+         * and returns false if not */
+        bool isSpace(char s) { return (s == ' ' || s == '\t' || s == '\n'); }
+
+        /*This function represents the scanner, it scans the input string character by character, and classifies the 
+         * tokens according to each token type and token value */
+       
         public Form1()
         {
             InitializeComponent();
         }
-
+        OpenFileDialog ofd = new OpenFileDialog();
         private void button1_Click(object sender, EventArgs e)
         {
            
-            OpenFileDialog ofd = new OpenFileDialog();
+            
             ofd.ShowDialog();
             string x = ofd.FileName;
-
-             int n = 0;
+            string before=null;
+            int n = 0;
              string lines;
             string[] lines1 = System.IO.File.ReadAllLines(@x);
-            string before=null;
+            
             for (int i = 0; i < lines1.Length; i++)
             {
                 before += lines1[i] + Environment.NewLine;
-               
+                after += lines1[i];
             }
             richTextBox1.Text = before;
             /* StreamReader file = new StreamReader(@x);
@@ -53,6 +83,223 @@ namespace WindowsFormsApp1
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
            
+
+        }
+        public string getToken(string input )
+        {
+            state = states.START;
+            // s Reserve;
+            bool flag = false;
+            string[] Reserve = new string[] { "if", "then", "else", "end", "repeat", "until", "read", "write" };
+            string token = null;
+            string token1 = null;
+            while (state != states.DONE && state != states.ERROR)
+            {
+                switch (state)
+                {
+                    case states.START:
+                        if (isSpace(input[j]))
+                        {
+                            /* if scanned character is white space then ignore it and go to scan the next character
+                             as long as the input has not finished,
+                            But if this white space is the last character in the input string then go state DONE*/
+                            j++;
+                            if (j == input.Length)
+                            {
+                                state = states.DONE;
+                            }
+                            else
+                            {
+                                state = states.START;
+                            }
+
+                        }
+                        else if (isDigit(input[j]))
+                        {
+                            /*if scanned character is digit then go to state NUM*/
+                            state = states.NUM;
+                        }
+                        else if (isLetter(input[j]))
+                        {
+                            /*if scanned character is letter then go to state IDENTIFIER*/
+                            state = states.IDINTIFIER;
+                        }
+                        else if (isSymbol(input[j]))
+                        {
+                            /*if scanned character is symbol then print it as a string value of 
+                             * a token of token type Special Symbols*/
+
+                            if (input[j] == '=')
+                            {
+                                if (input[j - 1] != ':')
+                                {
+                                    token1 = input[j] + " , Symbol \n";
+                                }
+                            }
+                            else
+                            {
+                                token1 = input[j] + " , Symbol \n";
+                            }
+
+                            /*if this symbol is the last one in the input string then go to state DONE,
+                             if not then go to scan the next character*/
+                            j++;
+                            //if (j == input.Length)
+                                state = states.DONE;
+
+
+                        }
+                        /* increment the j to scan the next character in the input string*/
+                        else if (input[j] == ':')
+                        {
+                            j++;
+                            /*if the scanned character ':'  is the last character in the input string then go to state ERROR*/
+                            if (j == input.Length)
+                                state = states.ERROR;
+
+                            /*if the scanned character ':'  is not the last character then go to state ASSIGN*/
+                            else
+                                state = states.ASSIGN;
+
+                        }
+                        else if (input[j] == '{')
+                        {
+                            /*if scanned character is ':' then go to state COMMENT*/
+                            state = states.COMMENT;
+                        }
+                        else
+                        {
+                            /*if the scanned character is [other] then go to state DONE */
+                            state = states.DONE;
+                        }
+                        break;
+
+                    case states.ASSIGN:
+                        if (input[j] == '=')
+                        {
+                            state = states.DONE;
+                            token = ":=";
+                            token1 = token + ",Assign\n";
+                        }
+                        else
+                        {
+                            state = states.ERROR;
+                        }
+                        break;
+                    case states.IDINTIFIER:
+
+                        {
+
+                            while ( (j != input.Length) && (isLetter(input[j])))
+                            {
+                                token += input[j];
+                                j++;
+                                /*if (j == input.Length)
+                                {
+                                    state = states.DONE;
+                                    break;
+                                }*/
+                            }
+
+                            //else state = states.DONE;
+                        }
+                        for (int i = 0; i < 8; i++)
+                        {
+                            if (token == Reserve[i])
+                            {
+                                token1 =
+                                   token + " , reserved word \n";
+                                flag = true;
+                                break;
+                            }
+                            
+                        }
+                        if(flag==false)
+                        {
+                            token1 =
+                            token + " ,  identifier \n";
+                        }
+                        
+                         state = states.DONE;
+                        //else state = states.START;
+
+                        break;
+                    //IN CASE INPUT IS BETWEEN { }
+                    //COMMENT CASE
+
+                    case states.COMMENT:
+
+                        if (state == states.COMMENT)
+                        {
+                            while (true)
+                            {
+                                j++;
+                                if (input[j] == '}')
+                                {
+                                    j++;
+                                    break;
+                                }
+                            }
+                            if (j  == input.Length)
+                            {
+                                /* go to done state */
+                                state = states.DONE;
+                            }
+                            else
+                            {
+                                /* return to start state */
+                                state = states.START;
+                            }
+
+                        }
+                        break;
+
+                    //IN CASE INPUT IS DIGIT
+                    //NUM CASE
+
+                    case states.NUM:
+
+                        while ((j != input.Length) && isDigit(input[j]))
+                        {
+                            token += input[j];
+                            j++;
+                        }
+
+                        //label1.Text += mytoken += " , IS NUMBER \n";
+                        token1 = token + " , NUMBER \n";
+                        //token = "";
+                        
+                            /* go to done state */
+                            state = states.DONE;
+                       
+                        
+                           
+                        
+                        break;
+                    case states.DONE:
+                        break;
+                }
+            }
+            return token1;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //state = states.IDINTIFIER;
+            //string x = "{ Sample program in TINY language – computes factorial } read x;{ input an integer }if  0 < x   then     { don’t compute if x <= 0 }fact:= 1; repeat fact  := fact * x  x:= x - 1 until x = 0; write fact { output factorial of x }end";
+            string str=null;
+            
+            while ( j!=after.Length) 
+            {
+                str+=getToken(after) ; 
+                
+            }
+            richTextBox2.Text = str;
 
         }
     }
