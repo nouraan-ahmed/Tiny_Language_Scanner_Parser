55c41d3481fb2027ab9c2469f56d2e42e8ae9249
nehalyasser <64105565+nehalyasser@users.noreply.github.com>
2021-11-09 22:54:37 +0200

    Start,Assign&Error states

--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ namespace ConsoleApp
     class Program
     {
         /******************** Types Declarations******************/
-       enum states { START, COMMENT, NUM, IDINTIFIER, ASSIGN, DONE };
+       enum states { START, COMMENT, NUM, IDINTIFIER, ASSIGN, DONE, ERROR };
+
+        /******************** Global Variables******************/
+        states state = states.START;
 
         /********************Function Definitions****************/
-        
+
         /* if the input is withen the range of digits (0 to 9) this function returns true,
          * and returns false if not */
         bool isDigit(char d) { return (d >= '0' && d <= '9'); }
@@ -32,6 +35,89 @@ namespace ConsoleApp
          * tokens according to each token type and token value */
         public void getToken (string input)
         {
+            string token;
+            while (state!=states.DONE)
+            {
+                int counter = 0;
+                switch (state)
+                {
+                    case states.START:
+                        if (isSpace(input[counter]))
+                        {
+                            /* if scanned character is white space then ignore it and go to scan the next character
+                             as long as the input has not finished,
+                            But if this white space is the last character in the input string then go state DONE*/
+                            counter++;
+                            if (counter == input.Length)
+                                state = states.DONE;
+
+                        }
+                        else if (isDigit(input[counter]))
+                        {
+                            /*if scanned character is digit then go to state NUM*/
+                            state = states.NUM;
+                        }
+                        else if (isLetter(input[counter]))
+                        {
+                            /*if scanned character is letter then go to state IDENTIFIER*/
+                            state = states.IDINTIFIER;
+                        }
+                        else if (isSymbol(input[counter]))
+                        {
+                            /*if scanned character is symbol then print it as a string value of 
+                             * a token of token type Special Symbols*/
+
+                            // N7OTAHA LMA N&OT DA 3AL GUI
+
+                            /*if this symbol is the last one in the input string then go to state DONE,
+                             if not then go to scan the next character*/
+                            counter++;
+                            if (counter == input.Length)
+                                state = states.DONE;
+                            
+
+                        }
+                        /* increment the counter to scan the next character in the input string*/
+                        else if (input[counter]==':')
+                        {
+                            counter++;
+                            /*if the scanned character ':'  is the last character in the input string then go to state ERROR*/
+                            if (counter == input.Length)
+                                state = states.ERROR;
+
+                            /*if the scanned character ':'  is not the last character then go to state ASSIGN*/
+                            else 
+                                state = states.ASSIGN;
+                            
+                        }
+                        else if (input[counter]=='{')
+                        {
+                            /*if scanned character is ':' then go to state COMMENT*/
+                            state = states.COMMENT;
+                        }
+                        else
+                        {
+                            /*if the scanned character is [other] then go to state DONE */
+                            state = states.DONE;
+                        }
+                        break;
+
+                    case states.ASSIGN:
+                        if(input[counter]=='=')
+                        {
+                            state = states.DONE;
+                        }
+                        else
+                        {
+                            state = states.ERROR;
+                        }
+                        break;
+
+
+                    
+                }
+            }
+            
 
         }
         static void Main(string[] args)
