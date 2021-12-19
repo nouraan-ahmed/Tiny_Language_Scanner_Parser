c4b990fac4b68de58b1f1e8fa62a3f1556f4cb74
Heba Atef <hoba30.ha@gmail.com>
2021-12-19 20:06:20 +0200

    organize folders, add tree & token files in parser

---
+++ Parser/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tiny_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+
+            Token token1 = new Token("X", "IDENTIFIER");
+            Token token2 = new Token("if", "IF");
+            Token token3 = new Token("5", "NUMBER");
+            Token token4 = new Token("=", "EQUAL");
+            Token token5 = new Token("read", "READ");
+            Token token6 = new Token("Y", "IDENTIFIER");
+
+            Node root = tree.getTreeRoot();
+            Node ifnode = new Node(token2);
+            Node opnode = new Node(token4);
+            Node op1node = new Node(token1);
+            Node op2node = new Node(token3);
+
+            Node ifbody_read = new Node(token5);
+            Node readid = new Node(token6);
+
+
+            tree.appendChild(root, ifnode);
+            tree.appendChild(ifnode, opnode);
+            tree.appendChild(opnode, op1node);
+            tree.appendChild(opnode, op2node);
+            tree.appendChild(ifnode, ifbody_read);
+            tree.appendChild(ifbody_read, readid);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Parser/Tree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiny_Parser
+{
+    class Node
+    {
+        private Token token;
+        private List<Node> children = new List<Node>();
+
+        public Node(Token t)
+        {
+            token = t;
+        }
+        public Node()
+        {
+            token = null;
+        }
+
+        public List<Node> getChildren()
+        {
+            return children;
+        }
+
+        public Token getToken()
+        {
+            return token;
+        }
+
+        public void setToken(Token t)
+        {
+            token = t;
+        }
+
+    }
+    class Tree
+    {
+        private Node root;
+        public Tree(Node root)
+        {
+            this.root = root;
+        }
+
+        public Tree()
+        {
+            Token program_token = new Token("Program", "PROGRAM");
+            root = new Node(program_token);
+        }
+
+        public Node getTreeRoot()
+        {
+            return this.root;
+        }
+        
+        public void appendChild(Node parent, Node child)
+        {
+            parent.getChildren().Add(child);
+        }
+    }
+}
