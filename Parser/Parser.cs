743c952b3a22e19ebc4e2bb6f4013fbb7f866424
nehalyasser <nehalyasser1999@gmail.com>
2021-12-23 01:58:33 +0200

    Parser class

---
+++ Parser/Parser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tiny_Parser
+{
+    internal class Parser
+    {
+        Stack<Token> stack = new Stack<Token>();
+        public Stack<Token> pushTokensToStack(List<Token>tokensList)
+        {
+            for(int i= tokensList.Count; i>0;i--)
+            {
+                stack.Push(tokensList[i]);
+            }
+            return stack;
+        }
+    }
+}
