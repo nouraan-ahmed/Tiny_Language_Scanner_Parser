97200c9e0da39d14246b7ea4682a2f58a6a6d8d7
Heba Atef <hoba30.ha@gmail.com>
2021-12-22 13:28:43 +0200

    changes to tree & token

--- Parser/Tree.cs
+++ Parser/Tree.cs
@@ -10,7 +10,8 @@ namespace Tiny_Parser
     {
         private Token token;
         private List<Node> children = new List<Node>();
-
+        private int left_margin = 0;
+        private int child_no = 0;
         public Node(Token t)
         {
             token = t;
@@ -25,6 +26,28 @@ namespace Tiny_Parser
             return children;
         }
 
+        public int getChildrenCount()
+        {
+            return children.Count();
+        }
+
+        public int getLeftMargin()
+        {
+            return left_margin;
+        }
+        public void setLeftMargin(int l)
+        {
+            left_margin = l;
+        }
+
+        public int getChildNo()
+        {
+            return child_no;
+        }
+        public void setChildNo(int n)
+        {
+            child_no = n;
+        }
         public Token getToken()
         {
             return token;
@@ -59,5 +82,30 @@ namespace Tiny_Parser
         {
             parent.getChildren().Add(child);
         }
+
+        public void traverseTree(Action<Node, Node, int> drawNode)
+        {
+            treverseTreeAUX(root, drawNode, -1);
+        }
+
+        private void treverseTreeAUX(Node n, Action<Node, Node, int> drawNode, int depth)
+        {
+            if (n.getChildrenCount() == 0)
+            {
+                return;
+            }
+
+            depth++;
+            //print child
+            //traverse this child
+            List<Node> children = n.getChildren();
+            for (int i = 0; i < children.Count(); i++)
+            {
+                children[i].setChildNo(i);
+                drawNode(n, children[i], depth);
+                treverseTreeAUX(children[i], drawNode, depth);
+            }
+        }
+
     }
 }
