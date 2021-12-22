572e1aa7916ba8c9c6d8d5e82554e4ffe7e626f0
Heba Atef <hoba30.ha@gmail.com>
2021-12-23 01:24:16 +0200

    added drawing clas

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -17,58 +17,20 @@ using System.Windows.Shapes;
 
 namespace Tiny_Parser
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         int page_number = 0;
         string sourceDirectory = null;
         string inputFileName = null;
         string outputFile = null;
+        Boolean isDrawn = false;
 
         public MainWindow()
         {
             InitializeComponent();
-
             //hide buttons
             Hide_Page1();
-        }
-
-        private void Parser_Click(object sender, RoutedEventArgs e)
-        {
-            page_number = 1;
-            //hide the items of main page
-            Hide_Main_Page();
-
-            //show items of input page
-            Show_Page1();
-        }
-
-        private void Back_Click(object sender, RoutedEventArgs e)
-        {
-            if(page_number == 1)
-            {
-                page_number--;
-                //go back to the main page
-                Hide_Page1();
-
-                Show_Main_Page();
-
-            }
-            else if(page_number == 2)
-            {
-                page_number--;
-                Hide_Page2();
-                Show_Page1();
-            }
-        }
-
-        private void Syntax_Tree_Click(object sender, RoutedEventArgs e)
-        {
-            page_number = 2;
-            Hide_Page1();
-            Show_Page2();
+            Hide_Page2();
         }
 
         private void Browse_Click(object sender, RoutedEventArgs e)
@@ -90,26 +52,175 @@ namespace Tiny_Parser
                 //Get the path of specified file
                 var filePath = input_file.FileName;
                 inputFileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
-
-                //get the directory name the file is in
-                sourceDirectory = System.IO.Path.GetDirectoryName(filePath);
-
-                //Read the contents of the file into a stream
-                var fileStream = input_file.OpenFile();
-                using (StreamReader reader = new StreamReader(fileStream))
+                
+                //check if the chosen file is empty
+                long fileLen = new FileInfo(filePath).Length;
+                if (fileLen == 0 || (fileLen == 3 && File.ReadAllBytes("file").SequenceEqual(new byte[] { 239, 187, 191 })))
                 {
-                    var fileContent = reader.ReadToEnd();
-                    input_text.Text = fileContent;
-
-                    //read from the begginig of the file
-                    reader.DiscardBufferedData();
-                    reader.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
-                    reader.Close();
+                    /* Is empty */
+                    MessageBox.Show("An empty file can not be scanned.\nPlease, choose a non empty file", "Browse error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-                scan.Visibility = Visibility.Visible;
+
+                else
+                {
+                    syntax_tree.Visibility = Visibility.Hidden;
+                    isDrawn = false;
+                    //get the directory name the file is in
+                    sourceDirectory = System.IO.Path.GetDirectoryName(filePath);
+
+                    //Read the contents of the file into a stream
+                    var fileStream = input_file.OpenFile();
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        var fileContent = reader.ReadToEnd();
+                        input_text.Text = fileContent;
+
+                        //read from the begginig of the file
+                        reader.DiscardBufferedData();
+                        reader.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+                        reader.Close();
+                    }
+                    scan.Visibility = Visibility.Visible;
+                }
             }
         }
 
+        private void Scaner_Click(object sender, RoutedEventArgs e)
+        {
+            syntax_tree.Visibility = Visibility.Visible;
+
+        }
+
+        private void Syntax_Tree_Click(object sender, RoutedEventArgs e)
+        {
+            isDrawn = true;
+            page_number = 2;
+            Hide_Page1();
+            Show_Page2();
+
+            Tree tree = new Tree();
+            Token token1 = new Token("read", "READ");
+            Token token2 = new Token("X", "IDENTIFIER");
+            Node root = tree.getTreeRoot();
+            Node n1 = new Node(token1);
+            Node n2 = new Node(token2);
+            tree.appendChild(root, n1);
+            tree.appendChild(n1, n2);
+
+            Token token3 = new Token("if", "IF");
+            Token token4 = new Token("<", "LESSTHAN");
+            Token token5 = new Token("0", "NUMBER");
+            Token token6 = new Token("X", "IDENTIFIER");
+            Node n3 = new Node(token3);
+            Node n4 = new Node(token4);
+            Node n5 = new Node(token5);
+            Node n6 = new Node(token6);
+            tree.appendChild(root, n3);
+            tree.appendChild(n3, n4);
+            tree.appendChild(n4, n5);
+            tree.appendChild(n4, n6);
+
+            Token token7 = new Token("assign", "ASSIGN");
+            Token token8 = new Token("fact", "IDENTIFIER");
+            Token token9 = new Token("1", "NUMBER");
+            Node n7 = new Node(token7);
+            Node n8 = new Node(token8);
+            Node n9 = new Node(token9);
+            tree.appendChild(n3, n7);
+            tree.appendChild(n7, n8);
+            tree.appendChild(n7, n9);
+
+            Token token10 = new Token("repeat", "REPEAT");
+
+            Token token11 = new Token("assign", "ASSIGN");
+            Token token12 = new Token("fact", "IDENTIFIER");
+            Token token13 = new Token("*", "MULT");
+            Token token14 = new Token("fact", "IDENTIFIER");
+            Token token15 = new Token("x", "IDENTIFIER");
+
+            Token token16 = new Token("assign", "ASSIGN");
+            Token token17 = new Token("x", "IDENTIFIER");
+            Token token18 = new Token("-", "MINUS");
+            Token token19 = new Token("x", "IDENTIFIER");
+            Token token20 = new Token("1", "NUMBER");
+
+            Token token21 = new Token("=", "EQUAL");
+            Token token22 = new Token("x", "IDENTIFIER");
+            Token token23 = new Token("0", "NUMBER");
+
+            Node n10 = new Node(token10);
+            Node n11 = new Node(token11);
+            Node n12 = new Node(token12);
+            Node n13 = new Node(token13);
+            Node n14 = new Node(token14);
+            Node n15 = new Node(token15);
+            Node n16 = new Node(token16);
+            Node n17 = new Node(token17);
+            Node n18 = new Node(token18);
+            Node n19 = new Node(token19);
+            Node n20 = new Node(token20);
+            Node n21 = new Node(token21);
+            Node n22 = new Node(token22);
+            Node n23 = new Node(token23);
+            tree.appendChild(n3, n10);
+            tree.appendChild(n10, n11);
+            tree.appendChild(n10, n16);
+            tree.appendChild(n10, n21);
+
+            tree.appendChild(n11, n12);
+            tree.appendChild(n11, n13);
+            tree.appendChild(n13, n14);
+            tree.appendChild(n13, n15);
+            tree.appendChild(n16, n17);
+            tree.appendChild(n16, n18);
+            tree.appendChild(n18, n19);
+            tree.appendChild(n18, n20);
+
+            tree.appendChild(n21, n22);
+            tree.appendChild(n21, n23);
+
+            Token token24 = new Token("write", "WRITE");
+            Token token25 = new Token("fact", "IDENTIFIER");
+            Node n24 = new Node(token24);
+            Node n25 = new Node(token25);
+            tree.appendChild(n3, n24);
+            tree.appendChild(n24, n25);
+
+            DrawSyntaxTree d = new DrawSyntaxTree(syntax_tree_canvas);
+            tree.traverseTree(d.Draw_Child_Node);
+
+        }
+
+        // GUI Styling Functions // 
+        private void Parser_Click(object sender, RoutedEventArgs e)
+        {
+            page_number = 1;
+            //hide the items of main page
+            Hide_Main_Page();
+
+            //show items of input page
+            Show_Page1();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (page_number == 1)
+            {
+                page_number--;
+                //go back to the main page
+                Hide_Page1();
+                Show_Main_Page();
+            }
+
+            else if (page_number == 2)
+            {
+                page_number--;
+                Hide_Page2();
+                Show_Page1();
+            }
+        }
+
+        // Multi-page manipulation functions
         private void Show_Main_Page()
         {
             logo.Visibility = Visibility.Visible;
@@ -124,11 +235,14 @@ namespace Tiny_Parser
             parser.Visibility = Visibility.Hidden;
         }
 
-
         private void Show_Page1()
         {
             back.Visibility = Visibility.Visible;
             //syntax_tree.Visibility = Visibility.Visible;
+            if (isDrawn)
+            {
+                syntax_tree.Visibility = Visibility.Visible;
+            }
 
             header2.Visibility = Visibility.Visible;
             browse.Visibility = Visibility.Visible;
@@ -153,25 +267,23 @@ namespace Tiny_Parser
             output_text.Visibility = Visibility.Hidden;
         }
 
-
         private void Show_Page2()
         {
             syntax_tree.Visibility = Visibility.Hidden;
             back.Visibility = Visibility.Visible;
             header3.Visibility = Visibility.Visible;
+            syntax_tree_canvas.Visibility = Visibility.Visible;
+            scroll.Visibility = Visibility.Visible;
         }
 
         private void Hide_Page2()
         {
             header3.Visibility = Visibility.Hidden;
+            syntax_tree_canvas.Visibility = Visibility.Hidden;
+            scroll.Visibility = Visibility.Hidden;
         }
 
-        private void Scaner_Click(object sender, RoutedEventArgs e)
-        {
-            syntax_tree.Visibility = Visibility.Visible;
-
-        }
-
+        // To overcome the default effect of WPF when hovering over a button
         private void Button_Hover(object sender, MouseEventArgs e)
         {
             (sender as Button).Foreground = Brushes.Black;
---
+++ GUI/classes/DrawSyntaxTree.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Tiny_Parser
+{
+    class DrawSyntaxTree
+    {
+        //int left_margin = 20;
+        int top_margin = 70;
+        //int line_length = 15;
+        Canvas MyCanvas;
+
+        int child_left_margin = 0;
+        int last_left_margin = 0;
+        int last_top_margin = 0;
+        Boolean isChild = true;
+
+        public DrawSyntaxTree(Canvas c)
+        {
+            MyCanvas = c;
+        }
+
+        public void Draw_Child_Node(Node parent, Node child, int depth)
+        {
+            Token token = child.getToken();
+            int children_count = parent.getChildrenCount();
+            
+            //draw a line 
+            Line line = new Line
+            {
+                StrokeThickness = 1,
+                Stroke = System.Windows.Media.Brushes.Black,
+            };
+
+            Line horizontal_line = new Line
+            {
+                StrokeThickness = 1,
+                Stroke = System.Windows.Media.Brushes.Black,
+            };
+
+            if (children_count == 1 && depth != 0)
+            {
+                //left margin of this only child equals the parent left margin
+                child_left_margin = parent.getLeftMargin();
+            }
+
+            else if(children_count == 2 && depth != 0 && parent.getToken().Tokentype != "ASSIGN")
+            {
+                //left margin of two children ,, the first is behind the parent and the second is ahead of the parent
+                //but for the first node in the tree with children then make the first child have the same left margin
+                //so that it wont get out of the window frame
+                switch(child.getChildNo())
+                {
+                    case 0:
+                        child_left_margin = (depth != 0) ? parent.getLeftMargin() - 50 : parent.getLeftMargin();
+                        break;
+
+                    case 1:
+                        child_left_margin = (depth != 0) ? parent.getLeftMargin() + 50 : parent.getLeftMargin() + 100;
+                        break;
+                }
+            }
+
+            else if (children_count == 3 && depth != 0)
+            {
+                switch (child.getChildNo())
+                {
+                    case 0:
+                        child_left_margin = (depth == 0) ? parent.getLeftMargin() - 80: parent.getLeftMargin();
+                        break;
+
+                    case 1:
+                        child_left_margin = (depth == 0) ? parent.getLeftMargin() : parent.getLeftMargin() + 200;
+                        break;
+
+                    case 2:
+                        child_left_margin = (depth == 0) ? parent.getLeftMargin() + 80 : parent.getLeftMargin() + 400;
+                        break;
+                }
+            }
+
+            else if (child.getChildNo() == 0 || (parent.getToken().Tokentype == "ASSIGN" && child.getChildNo() == 1))
+            {
+                child_left_margin = last_left_margin;
+            }
+
+            else
+            {
+                child_left_margin = last_left_margin + 100;
+            }
+            //assign & read have their children inside the box not as a child
+            if (isExpression(child) && parent.getToken().Tokentype != "READ" && (parent.getToken().Tokentype != "ASSIGN" || (parent.getToken().Tokentype == "ASSIGN" && child.getChildNo() > 0)))
+            {
+                //draw oval shape
+                Border oval = new Border
+                {
+                    CornerRadius = new CornerRadius(50),
+                    Width = 80,
+                    Height = 50,
+                    Margin = new Thickness { Left = child_left_margin, Top = top_margin * depth},
+                    Padding = new Thickness(4),
+                    BorderBrush = new SolidColorBrush(Colors.Black),
+                    BorderThickness = new Thickness(1),
+                };
+
+                child.setLeftMargin((int)oval.Margin.Left);
+
+                TextBlock oval_content = new TextBlock
+                {
+                    TextAlignment = TextAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontSize = 15,
+                };
+
+                switch(token.Tokentype)
+                {
+                    case "IDENTIFIER":
+                        oval_content.Text = "id\n(" + token.Tokenvalue + ")";
+                        break;
+
+                    case "NUMBER":
+                        oval_content.Text = "const\n(" + token.Tokenvalue + ")";
+                        break;
+
+                    default:
+                        oval_content.Text = "op\n(" + token.Tokenvalue + ")";
+                        break;
+                }
+
+                oval.Child = oval_content;
+
+                //all oval shapes are connected to their parents
+                line.X2 = (oval.Width / 2) + oval.Margin.Left;
+                line.Y2 = oval.Margin.Top;
+
+                line.X1 = (oval.Width / 2) + parent.getLeftMargin();
+                line.Y1 = top_margin * (depth - 1) + oval.Height;
+                
+
+                MyCanvas.Children.Add(oval);
+                last_top_margin = (int)oval.Margin.Top;
+            }
+
+            else if ((parent.getToken().Tokentype != "ASSIGN" || (parent.getToken().Tokentype == "ASSIGN" && child.getChildNo() > 0)) && parent.getToken().Tokentype != "READ")
+            {
+                // Create rectangle.
+                Border rectangle = new Border
+                {
+                    Width = 70,
+                    Height = 50,
+                    Margin = new Thickness { Left = child_left_margin, Top = top_margin * depth },
+                    Padding = new Thickness(4),
+                    BorderBrush = new SolidColorBrush(Colors.Black),
+                    BorderThickness = new Thickness(1),
+                };
+
+                child.setLeftMargin((int)rectangle.Margin.Left);
+
+                TextBlock rect_content = new TextBlock
+                {
+                    TextAlignment = TextAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontSize = 15,
+                };
+
+                //to write the read/assign first child inside its box
+                switch (token.Tokentype)
+                {
+                    case "READ":
+                        rect_content.Text = "read\n(" + child.getChildren()[0].getToken().Tokenvalue + ")";
+                        break;
+
+                    case "ASSIGN":
+                        rect_content.Text = "assign\n(" + child.getChildren()[0].getToken().Tokenvalue + ")";
+                        break;
+
+                    default:
+                        rect_content.Text = token.Tokenvalue;
+                        break;
+                }
+
+                rectangle.Child = rect_content;
+
+                if((depth == 0 && child.getChildNo() > 0) || (parent.getToken().Tokentype == "IF" && !child.getToken().isElsePart && child.getChildNo() > 1) || (parent.getToken().Tokentype == "REPEAT" && child.getChildNo() > 0))
+                {
+                    horizontal_line.X2 = rectangle.Margin.Left;
+                    horizontal_line.Y2 = (rectangle.Height / 2) + rectangle.Margin.Top;
+
+                    horizontal_line.X1 = rectangle.Width + parent.getChildren()[child.getChildNo() - 1].getLeftMargin();
+                    horizontal_line.Y1 = top_margin * depth + (rectangle.Height / 2);
+                }
+
+                else
+                {
+                    line.X2 = (rectangle.Width / 2) + rectangle.Margin.Left;
+                    line.Y2 = rectangle.Margin.Top;
+
+                    line.X1 = (rectangle.Width / 2) + parent.getLeftMargin();
+                    line.Y1 = top_margin * (depth - 1) + rectangle.Height;
+                }
+
+                MyCanvas.Children.Add(rectangle);
+                last_top_margin = (int)rectangle.Margin.Top;
+            }
+
+            last_left_margin = child_left_margin;
+
+            //in order not to draw the line between the root node which is not drawn in the syntax tree & its children
+            if(depth != 0) MyCanvas.Children.Add(line);
+            MyCanvas.Children.Add(horizontal_line);
+
+            MyCanvas.Width = last_left_margin + 80 + 30;
+            MyCanvas.Height = last_top_margin + 50 + 30;
+        }
+
+
+        private Boolean isExpression(Node n)
+        {
+            string tokenType = n.getToken().Tokentype;
+            switch(tokenType)
+            {
+                case "NUMBER":
+                    break;
+
+                case "IDENTIFIER":
+                    break;
+
+                case "LESSTHAN":
+                    break;
+
+                case "EQUAL":
+                    break;
+
+                case "PLUS":
+                    break;
+
+                case "MINUS":
+                    break;
+
+                case "MULT":
+                    break;
+
+                case "DIV":
+                    break;
+
+                default:
+                    return false;
+            }
+            return true;
+        }
+    }
+}
